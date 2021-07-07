8f38f0865b50c986a5548c27d37d2611683138eb
liaojh <liaojh@augurit.com>
2021-07-07 14:14:21 +0800

    build: 安装引入sass-loader、normalize.css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from "./store";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI)
